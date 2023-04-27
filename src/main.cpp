b746d0734b82eb61493c9dbd5e7e3e5b663146e0
coco875 <59367621+coco875@users.noreply.github.com>
2023-04-27 23:09:02 +0200

    remade errror.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@
 #include <cstdint>
 #include <cstdio>
 
-#include <Error.hpp>
-#include <options.h>
-#include <custom_exceptions.h>
+#include "error.hpp"
+#include "options.hpp"
 
 using namespace std;
 using combinations = vector<vector<string>>;
@@ -25,8 +24,7 @@ void copy(string filename, ofstream &output) {
         }
         output << endl;
     } else {
-        Error err(ErrorType::FILE_NOT_FOUND, filename);
-        err.throw_error();
+        throw FileNotFound(filename);
     }
 }
 
@@ -91,7 +89,7 @@ inline vector<string> get_inside_brackets( const vector<string>& tree, size_t & 
     
     while(level > 0) {
         i++;
-        if(i >= tree.size()) Error(ErrorType::INVALID_SYNTAX, "Missing bracket !").throw_error();
+        if(i >= tree.size()) throw InvalidSyntax(i, "Missing closing bracket");
         if(tree[i] == open_bracket) level++;
         if(tree[i] == closed_bracket) level--;
         if(level != 0) inside_brackets.push_back(tree[i]);
@@ -160,19 +158,19 @@ string generateSimpleRulePopFunction(const vector<string> & rule, const string n
             */
             switch( x[i][0] ){      
                 case '{' :
-                    if(x[i].back() != '}') Error(ErrorType::INVALID_SYNTAX, "Missing '}'").throw_error();
+                    if(x[i].back() != '}') throw InvalidSyntax("none", "Missing '}'");
                     prefix = "pop_while( " + POP_FUNCTION_PREFIX ; suffix = ", current)";
                     break;
                 case '<' :
-                    if(x[i].back() != '>') Error(ErrorType::INVALID_SYNTAX, "Missing '>'").throw_error();
+                    if(x[i].back() != '>') throw InvalidSyntax("none", "Missing '>'");
                     prefix = POP_FUNCTION_PREFIX; suffix = "(next(current))";
                     break;
                 case '"' :
-                    if(x[i].back() != '"'  || x[i].size() < 2 ) Error(ErrorType::INVALID_SYNTAX, "Missing '\"' : " + x[i] ).throw_error();
+                    if(x[i].back() != '"'  || x[i].size() < 2 ) throw InvalidSyntax("none", "Missing '\"'");
                     prefix = "pop_value(\""; suffix = "\", next(current))";
                     break;
                 default :
-                    Error(ErrorType::INVALID_SYNTAX, "Mauvais !").throw_error();
+                    throw InvalidSyntax("none","Invalid syntax");
             }
             result += prefix + x[i].substr(1, x[i].size() - 2) + suffix; 
 
@@ -188,27 +186,23 @@ string generateSimpleRulePopFunction(const vector<string> & rule, const string n
 
 void checkFile(string &filename, string &output, ifstream &file, ofstream &out) {
     if(output == "") {
-        Error err(ErrorType::NO_FILENAME_SPECIFIED, "output file");
-        err.throw_error();
+        throw NoFilenameSpecified("output file");
     }
 
     if(filename == "") {
-        Error err(ErrorType::NO_FILENAME_SPECIFIED, "input file");
-        err.throw_error();
+        throw NoFilenameSpecified("input file");
     }
 
     file = ifstream(filename);
     if(!file.is_open()) {
-        Error err(ErrorType::FILE_NOT_FOUND, filename);
-        err.throw_error();
+        throw FileNotFound(filename);
     }
 
     remove(output.c_str());
     out = ofstream(output, ios::app);
 
     if(!out.is_open()) {
-        Error err(ErrorType::FILE_NOT_FOUND, output);
-        err.throw_error();
+        throw FileNotFound(output);
     }
 
 }
@@ -238,14 +232,13 @@ vector<string> createLexemes(ofstream &outputFile, ifstream &inputFile, uint32_t
             if(match.str(1)[0] != '.')
                 lexeme_names.push_back(match.str(1));
         } else {
-            Error err(ErrorType::INVALID_SYNTAX, lineNum);
-            err.throw_error();
+            throw InvalidSyntax(lineNum, "Invalid lexeme declaration");
         }
     }
     outputFile << "}" << endl << endl;
 
     if(line != "---"){
-        Error(ErrorType::INVALID_SYNTAX, lineNum).throw_error();
+        throw InvalidSyntax(lineNum, "Expected '---'");
     }
     return lexeme_names;
 }
@@ -307,7 +300,7 @@ vector<pair<string, Rule>> readRules(ifstream &file, uint32_t& lineNum){
 
             if(!regex_search(line, match, rule_infos_regex)) { 
                 break;
-                // Error(ErrorType::INVALID_SYNTAX, lineNum).throw_error();
+                // throw InvalidSyntax(line, "Invalid rule definition");
             }
             const string rule_name = match.str(1);
             string rule_expr = match.str(2);
@@ -316,7 +309,7 @@ vector<pair<string, Rule>> readRules(ifstream &file, uint32_t& lineNum){
             while(line.find(';') > line.length()) {
                 lineNum++;
                 if(!getline(file, line)) {
-                    Error(ErrorType::INVALID_RULE, lineNum).throw_error();
+                    throw InvalidSyntax(line, "Expected ';'");
                 }
                 rule_expr += line;
             }
@@ -369,7 +362,7 @@ int main(int argc, char const *argv[]) {
 
     try {
         Options::handleOptions(argc, argv, cfg);
-    } catch (CustomExceptions::OptionError& except) {
+    } catch (OptionError& except) {
         std::cerr << "Option error :\n  " << except.what() << std::endl;
         exit(1);
     }
--- src/options.cpp
+++ src/options.cpp
@@ -1,8 +1,9 @@
-#include <options.h>
 #include <iostream>
-#include <custom_exceptions.h>
 #include <string>
 #include <vector>
+
+#include "options.hpp"
+#include "error.hpp"
 
 typedef std::vector<char const *> ParamList;
 
@@ -22,11 +23,11 @@ const OptionDescription* tryGettingOptionFromID(char const *id) {
 void tryOption(OptionDescription const *option_ptr, const std::string &option_id, const ParamList &param_list, Configuration &cfg) {
     try {
         (*option_ptr)(param_list, cfg);
-    } catch (CustomExceptions::OptionError& except) {
+    } catch (OptionError& except) {
         if (option_id == "") {
-            throw CustomExceptions::OptionError("T'as fait de la merde avec la default option");
+            throw OptionError("T'as fait de la merde avec la default option");
         } else {
-            throw CustomExceptions::OptionError("-" + option_id + ": " + except.what() + "\n  (type \".\\ggram --help " + option_id + "\" for more information on how to use this option)\n");
+            throw OptionError("-" + option_id + ": " + except.what() + "\n  (type \".\\ggram --help " + option_id + "\" for more information on how to use this option)\n");
         }
     }
 }
@@ -37,7 +38,7 @@ void handleOptions(int argc, char const *argv[], Configuration &cfg) {
     OptionDescription const *option_ptr = &defaultOption_description;
     ParamList param_list = {};
     
-    for(uint32_t i = 1; i != argc; ++i) { // TODO: why is it an uint32_t ?
+    for(int i = 1; i != argc; ++i) { // TODO: why is it an uint32_t ?
         char const * arg = argv[i];
         if (arg[0] == '-') {
             OptionDescription const *new_option_ptr = tryGettingOptionFromID(&arg[1]);
@@ -49,7 +50,7 @@ void handleOptions(int argc, char const *argv[], Configuration &cfg) {
                 option_ptr = new_option_ptr;
                 param_list = {};
             } else {
-                throw CustomExceptions::OptionError("Unknown option: " + std::string(arg));
+                throw OptionError("Unknown option: " + std::string(arg));
             }
         } else {
             param_list.push_back(arg);
@@ -61,7 +62,7 @@ void handleOptions(int argc, char const *argv[], Configuration &cfg) {
 
 void eddyMalou(const ParamList &param_list, Configuration &cfg) {
     if (param_list.size() != 0) {
-        throw CustomExceptions::OptionError("Too many parameters");
+        throw OptionError("Too many parameters");
     } else {
         std::cout << "On ne peut pas parler de politique administrative scientifique, le colloque à l'égard de la complexité doit vanter les encadrés avec la formule 1+(2x5), mais oui. Pour emphysiquer l'animalculisme, la congolexicomatisation par rapport aux diplomaties peut aider le conpemdium autour des gens qui connaissent beaucoup de choses, tu sais ça." << std::endl;
         exit(1);
@@ -70,14 +71,14 @@ void eddyMalou(const ParamList &param_list, Configuration &cfg) {
 
 void help(const ParamList &param_list, Configuration &cfg) {
     if (param_list.size() > 1) {
-        throw CustomExceptions::OptionError("Too many parameters");
+        throw OptionError("Too many parameters");
     } else if (param_list.size() == 1) {
 
         OptionDescription const *opt = tryGettingOptionFromID(param_list[0]);
         if(opt != nullptr) {
             opt->print();
         } else {
-            throw CustomExceptions::OptionError("Unknown option \"" + std::string(param_list[0]) + "\"");
+            throw OptionError("Unknown option \"" + std::string(param_list[0]) + "\"");
         }
         exit(1);
     } else {
@@ -90,7 +91,7 @@ void help(const ParamList &param_list, Configuration &cfg) {
 
 void version(const ParamList &param_list, Configuration &cfg) {
     if (param_list.size() != 0) {
-        throw CustomExceptions::OptionError("Too many parameters");
+        throw OptionError("Too many parameters");
     } else {
         std::cout << "version 0.0.1" << std::endl;
         exit(1);
@@ -99,21 +100,21 @@ void version(const ParamList &param_list, Configuration &cfg) {
 
 void inputFile(const ParamList &param_list, Configuration &cfg) {
     if (param_list.size() == 0) {
-        throw CustomExceptions::OptionError("Missing parameter");
+        throw OptionError("Missing parameter");
     } else if (param_list.size() == 1) {
         cfg.input_file = param_list[0];
     } else {
-        throw CustomExceptions::OptionError("Too many parameters");
+        throw OptionError("Too many parameters");
     }
 }
 
 void outputFile(const ParamList &param_list, Configuration &cfg) {
     if (param_list.size() == 0) {
-        throw CustomExceptions::OptionError("Missing parameter");
+        throw OptionError("Missing parameter");
     } else if (param_list.size() == 1) {
         cfg.input_file = param_list[0];
     } else {
-        throw CustomExceptions::OptionError("Too many parameters");
+        throw OptionError("Too many parameters");
     }
 }
 
