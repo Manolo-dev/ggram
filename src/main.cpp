e545b1b3276e265c7716f17cdbb26d1b92aca2cc
Manolo-dev <sardo.manolo.dev@gmail.com>
2023-04-27 16:55:50 +0200

    Revert "Revert "try to resolve the filesystem problem""
    This reverts commit 405df4081e2bc62f6fd935ce22662bd8cc34f144.

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,9 @@
 // Ca c'est la manip pour filesystem (mais on pourrait peut-être se limiter à experimental/filesystem ?)
 #if __has_include(<filesystem>)
   #include <filesystem>
-  namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
   #include <experimental/filesystem> 
-  namespace fs = std::experimental::filesystem;
+  using namespace std::experimental::filesystem;
 #else
   error "Missing the <filesystem> header."
 #endif
