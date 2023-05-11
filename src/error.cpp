74fcd4ee6c9a61f07d79a6cc8ff2c3505f351368
Robotechnic <robotechnic.developeur@gmail.com>
2023-05-11 11:52:19 +0200

    update format  to use spaces instead of tabs

--- src/error.cpp
+++ src/error.cpp
@@ -4,77 +4,72 @@
 GgramError::GgramError() : std::runtime_error::runtime_error("GgramError") {}
 
 const char *GgramError::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ---------------------- InvalidFileExtensionError ---------------------- //
 
-InvalidFileExtensionError::InvalidFileExtensionError(
-	const std::string & /*unused*/, const std::string &extension)
-	: _message("Invalid file extension. Only " + extension +
-			   " files are allowed.") {}
+InvalidFileExtensionError::InvalidFileExtensionError(const std::string & /*unused*/,
+                                                     const std::string &extension)
+    : _message("Invalid file extension. Only " + extension + " files are allowed.") {}
 
 const char *InvalidFileExtensionError::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ---------------------------- FileNotFound ----------------------------- //
 
 FileNotFound::FileNotFound(const std::string &filename)
-	: _message("File " + filename + " not found.") {}
+    : _message("File " + filename + " not found.") {}
 
 const char *FileNotFound::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ------------------------- NoFilenameSpecified ------------------------- //
 
 NoFilenameSpecified::NoFilenameSpecified(const std::string &type_file)
-	: _message("No " + type_file + " filename specified.") {}
+    : _message("No " + type_file + " filename specified.") {}
 
 const char *NoFilenameSpecified::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ----------------------------- RegexError ------------------------------ //
 
-RegexError::RegexError(const std::string &line, const std::string &regex,
-					   const std::string &error)
-	: _message("Regex error: " + error + " in line " + line + " with regex " +
-			   regex) {}
+RegexError::RegexError(const std::string &line, const std::string &regex, const std::string &error)
+    : _message("Regex error: " + error + " in line " + line + " with regex " + regex) {}
 
-RegexError::RegexError(unsigned int line, const std::string &regex,
-					   const std::string &error)
-	: _message("Regex error: " + error + " in line " + std::to_string(line) +
-			   " with regex " + regex) {}
+RegexError::RegexError(unsigned int line, const std::string &regex, const std::string &error)
+    : _message("Regex error: " + error + " in line " + std::to_string(line) + " with regex " +
+               regex) {}
 
 const char *RegexError::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ---------------------------- InvalidSyntax ---------------------------- //
 
 InvalidSyntax::InvalidSyntax(const std::string &line, const std::string &error)
-	: _message("Invalid syntax in line " + line + " : " + error) {}
+    : _message("Invalid syntax in line " + line + " : " + error) {}
 
 InvalidSyntax::InvalidSyntax(unsigned int line, const std::string &error)
-	: _message("Invalid syntax in line " + std::to_string(line) + " : " +
-			   error) {}
+    : _message("Invalid syntax in line " + std::to_string(line) + " : " + error) {}
 
 const char *InvalidSyntax::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ----------------------------- InvalidRule ----------------------------- //
 
 InvalidRule::InvalidRule(const std::string &line, const std::string &rule)
-	: _message("Invalid rule " + rule + " in line " + line) {}
+    : _message("Invalid rule " + rule + " in line " + line) {}
 
 InvalidRule::InvalidRule(unsigned int line, const std::string &rule)
-	: _message("Invalid rule " + rule + " in line " + std::to_string(line)) {}
+    : _message("Invalid rule " + rule + " in line " + std::to_string(line)) {}
 
 const char *InvalidRule::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ----------------------------- InputError ------------------------------ //
@@ -82,7 +77,7 @@ const char *InvalidRule::what() const noexcept {
 InputError::InputError(const std::string &message) : _message(message) {}
 
 const char *InputError::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
 
 // ------------------------------ BnfError ------------------------------- //
@@ -90,5 +85,5 @@ const char *InputError::what() const noexcept {
 BnfError::BnfError(const std::string &message) : _message(message) {}
 
 const char *BnfError::what() const noexcept {
-	return _message.c_str();
+    return _message.c_str();
 }
